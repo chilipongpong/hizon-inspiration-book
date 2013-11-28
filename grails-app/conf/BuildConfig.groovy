fcb486e632ade322a4240e0a092e1e01939cd369
Lorence del Rosario <lorence.delrosario@orangeandbronze.com>
2013-11-29 00:10:01 +0800

    fixed grails known issue

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -21,6 +21,7 @@ grails.project.fork = [
     console: [maxMemory: 768, minMemory: 64, debug: false, maxPerm: 256]
 ]
 
+grails.project.fork.run=true
 grails.project.dependency.resolver = "maven" // or ivy
 grails.project.dependency.resolution = {
     // inherit Grails' default dependencies
@@ -52,6 +53,7 @@ grails.project.dependency.resolution = {
     dependencies {
         // specify dependencies here under either 'build', 'compile', 'runtime', 'test' or 'provided' scopes e.g.
         // runtime 'mysql:mysql-connector-java:5.1.24'
+		build "org.fusesource.jansi:jansi:1.11"
 		
     }
 
